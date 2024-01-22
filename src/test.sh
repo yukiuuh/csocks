cc0ae3eefba8eb180f89dc71a8df7fec57fa887b
Yuki Hanayama <yhanayama@vmware.com>
2024-01-22 23:31:13 +0000

    test

--- src/test.sh
+++ src/test.sh
@@ -5,10 +5,13 @@ PROXY_PORT="1080"
 URL_DOMAIN_NAME="ifconfig.me"
 URL_IPV4="127.0.0.1:10000"
 
-TESTCASE[0]="curl -x socks4://$PROXY_ADDRESS:$PROXY_PORT $URL_DOMAIN_NAME"
-TESTCASE[1]="curl -x socks4://$PROXY_ADDRESS:$PROXY_PORT $URL_IPV4"
-TESTCASE[2]="curl -x socks5://$PROXY_ADDRESS:$PROXY_PORT $URL_DOMAIN_NAME"
-TESTCASE[3]="curl -x socks5://$PROXY_ADDRESS:$PROXY_PORT $URL_IPV4"
+TESTCASE=()
+TESTCASE+=("curl -x socks4://$PROXY_ADDRESS:$PROXY_PORT $URL_DOMAIN_NAME")
+TESTCASE+=("curl -x socks4://$PROXY_ADDRESS:$PROXY_PORT $URL_IPV4")
+TESTCASE+=("curl -x socks5://$PROXY_ADDRESS:$PROXY_PORT $URL_DOMAIN_NAME")
+TESTCASE+=("curl -x socks5://$PROXY_ADDRESS:$PROXY_PORT $URL_IPV4")
+TESTCASE+=("curl -x socks5h://$PROXY_ADDRESS:$PROXY_PORT $URL_DOMAIN_NAME")
+TESTCASE+=("curl -x socks5h://$PROXY_ADDRESS:$PROXY_PORT $URL_IPV4")
 
 for ((i = 0; i < ${#TESTCASE[@]}; i++)) ; do
     t="${TESTCASE[$i]}"; echo "====="; echo "test case: \`$t\`"; eval "$t"; echo; echo "====="; echo;
